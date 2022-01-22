fb378c45c47970dc605c2baa2c97fec8f6d28345
1InfinityDoesExist <patelavinash508@gmail.com>
2022-01-22 21:51:17 +0545

    longes common subsequence

--- src/Dynamic_Programming/MainClass.java
+++ src/Dynamic_Programming/MainClass.java
@@ -3,11 +3,17 @@ package Dynamic_Programming;
 public class MainClass {
 	public static void main(String[] args) {
 
-		int val[] = { 1, 4, 5, 7 };
-		int wt[] = { 1, 3, 4, 5 };
+//		int val[] = { 1, 4, 5, 7 };
+//		int wt[] = { 1, 3, 4, 5 };
+//
+//		KnapsackProblem kp = new KnapsackProblem();
+//		System.out.println(kp.knapsack(val, wt, 7));
 
-		KnapsackProblem kp = new KnapsackProblem();
-		System.out.println(kp.knapsack(val, wt, 7));
+		String str1 = "ABCDGHLQR";
+		String str2 = "AEDPHR";
+
+		LongestCommonSubsequence lcs = new LongestCommonSubsequence();
+		System.out.println(lcs.lcs(str1.toCharArray(), str2.toCharArray()));
 	}
 
 }
