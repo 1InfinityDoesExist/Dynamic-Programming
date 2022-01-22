cb514a306c4cc8f5bda1f467980e1a59d45e9104
1InfinityDoesExist <patelavinash508@gmail.com>
2022-01-22 21:05:32 +0545

    knapsack

--- src/Dynamic_Programming/KnapsackProblem.java
+++ src/Dynamic_Programming/KnapsackProblem.java
@@ -1,5 +1,8 @@
 package Dynamic_Programming;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class KnapsackProblem {
 
 	public int knapsack(int[] value, int[] weight, int W) {
@@ -11,21 +14,34 @@ public class KnapsackProblem {
 					dp[iter][jter] = 0;
 					continue;
 				}
-				if (weight[iter - 1] > jter) {
-					dp[iter][jter] = dp[iter - 1][jter];
-				} else {
+				if (weight[iter - 1] > jter) {// if weight is greater then j
+					dp[iter][jter] = dp[iter - 1][jter]; // take the value from top.
+				} else { // if weight is less than or equal to j (where j is weight from 0 to W)
 					dp[iter][jter] = Math.max(value[iter - 1] + dp[iter - 1][jter - weight[iter - 1]],
-							dp[iter - 1][jter]);
+							dp[iter - 1][jter]); // take max of Value of current weight + value of remaining weight and
+													// value by skipping that weight basically take the value from top
 				}
 			}
 		}
 
-		for (int iter = 0; iter < weight.length + 1; iter++) {
-			for (int jter = 0; jter < W + 1; jter++) {
-				System.out.print(dp[iter][jter] + " ");
+		// weight added in the sack.
+		int x = weight.length;
+		int y = W;
+		List<Integer> ans = new ArrayList<>();
+		while (true) {
+			if (dp[x][y] == 0) {
+				break;
 			}
-			System.out.println();
+			if (dp[x][y] == dp[x - 1][y]) {
+				x--;
+			} else {
+				ans.add(weight[x - 1]);
+				y = y - weight[x - 1];
+				x--;
+
+			}
 		}
+
 		return dp[weight.length][W];
 	}
 
